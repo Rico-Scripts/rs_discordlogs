e84c5ce43d570e4c8b73c6182a46cfbdb5a315bd
rico <r03292760@gmail.com>
2026-08-21 20:56:19 +0200

    feat: enable Discord Gateway presence

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,14 +3,16 @@ game 'gta5'
 
 author 'Rico-Scripts'
 description 'Universeel standalone Discord logging-systeem voor FiveM.'
-version '1.0.0'
+version '1.1.0'
 
 lua54 'yes'
+node_version '22'
 
 server_scripts {
     'config.lua',
     'server/utils.lua',
     'server/scanner.lua',
     'server/discord.lua',
-    'server/main.lua'
+    'server/main.lua',
+    'server/gateway.js'
 }
