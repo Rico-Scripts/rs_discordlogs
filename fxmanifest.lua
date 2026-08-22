19871b951211b14a0645f1f73098fd59c24b1fac
rico <r03292760@gmail.com>
2026-08-22 12:56:03 +0200

    feat: release hosted logger v3 manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,11 +2,10 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Rico-Scripts'
-description 'Universeel centraal Discord logging-systeem voor FiveM.'
-version '2.1.0'
+description 'Hosted centrale Discord logging voor FiveM via de officiele Rico Scripts bot.'
+version '3.0.0'
 
 lua54 'yes'
-node_version '22'
 
 server_scripts {
     'config.lua',
@@ -15,6 +14,5 @@ server_scripts {
     'server/discord.lua',
     'server/compat.lua',
     'server/adapters.lua',
-    'server/main.lua',
-    'server/gateway.js'
+    'server/main.lua'
 }
